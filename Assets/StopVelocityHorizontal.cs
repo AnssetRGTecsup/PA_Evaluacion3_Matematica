81009e0181c6eb971618ec0b877ddf26b75b834f
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2023-06-08 10:41:57 -0500

    DoneProyect
    Creo q ya esta, pero solo quedan 4 mins, ya cambia de color

--- Assets/StopVelocityHorizontal.cs
+++ Assets/StopVelocityHorizontal.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StopVelocityHorizontal : MonoBehaviour
 {
     [SerializeField] int currentModifier;
+    [SerializeField] MaterialController _materialController;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
@@ -12,14 +13,17 @@ public class StopVelocityHorizontal : MonoBehaviour
             if (currentModifier==1)
             {
                 other.GetComponent<Rigidbody>().velocity = new Vector3(0, other.transform.position.y, 0);
+                _materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
             }
             if (currentModifier == 2)
             {
                 other.GetComponent<Rigidbody>().velocity = new Vector3(other.transform.position.x, 0, 0);
+                _materialController.ChangeEmissionColor(MaterialChange.OnOnlyHorizontal);
             }
             if (currentModifier == 3)
             {
                 other.GetComponent<Rigidbody>().useGravity = false;
+                _materialController.ChangeEmissionColor(MaterialChange.OnLooseGravity);
             }
         }
     }
