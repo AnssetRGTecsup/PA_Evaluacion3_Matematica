13d83d40af095e05d118a0ac086f4bd8f63c60d0
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2023-06-08 10:48:05 -0500

    ImplementacionLevel1&2

--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishController : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class FinishController : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")){
             Debug.Log("GANO!");
+            SceneManager.LoadScene("Level3");
         }
     }
 }
