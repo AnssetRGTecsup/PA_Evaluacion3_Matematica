0c1352cdbd2fa330f0490c71e6d73e6e73c5620d
IvanCacereZ <130480223+IvanCacereZ@users.noreply.github.com>
2023-06-08 10:15:24 -0500

    1.2
    Se añadio color a los modificadores y ahora cambian el color de la pelotita

--- Assets/Scripts/Modificadores/MidificadorManager.cs
+++ Assets/Scripts/Modificadores/MidificadorManager.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class MidificadorManager : MonoBehaviour
 {
+    [SerializeField] private MaterialController materialController;
     private void OnTriggerEnter(Collider other)
     {
         if(this.gameObject.tag == "Horizontal")
         {
             Debug.Log("cambio Horizontal");
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
             other.GetComponent<BallController>().ChangeVelocityHorizontal();
             Destroy(this.gameObject);
         }
         else if (this.gameObject.tag == "Vertical")
         {
             Debug.Log("cambio Vertical");
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyHorizontal);
             other.GetComponent<BallController>().ChangeVelocityVertical();
             Destroy(this.gameObject);
         }
         else if (this.gameObject.tag == "Gravity")
         {
             Debug.Log("cambio de gravedad");
+            materialController.ChangeEmissionColor(MaterialChange.OnLooseGravity);
+            materialController.ChangeEmissionColor(MaterialChange.Normal);
             other.GetComponent<BallController>().ChangeGravity();
             Destroy(this.gameObject);
         }
