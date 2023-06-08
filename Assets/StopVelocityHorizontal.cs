bccc28899a41da5e5581bc6126aa6c54b761c14b
AlvaroEspinozaZ <130480207+AlvaroEspinozaZ@users.noreply.github.com>
2023-06-08 10:36:29 -0500

    Level3Done
    el Level 4 es el level 1 , xq le puse modifiers al 1 2 y 3

--- Assets/StopVelocityHorizontal.cs
+++ Assets/StopVelocityHorizontal.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class StopVelocityHorizontal : MonoBehaviour
 {
+    [SerializeField] int currentModifier;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            other.GetComponent<Rigidbody>().velocity = new Vector3(0, other.transform.position.y, 0) ;
-
+            if (currentModifier==1)
+            {
+                other.GetComponent<Rigidbody>().velocity = new Vector3(0, other.transform.position.y, 0);
+            }
+            if (currentModifier == 2)
+            {
+                other.GetComponent<Rigidbody>().velocity = new Vector3(other.transform.position.x, 0, 0);
+            }
+            if (currentModifier == 3)
+            {
+                other.GetComponent<Rigidbody>().useGravity = false;
+            }
         }
     }
 }
