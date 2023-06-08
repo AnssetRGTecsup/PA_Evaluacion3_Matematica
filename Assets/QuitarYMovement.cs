1cf2f093ea581549bfffb706f8cf247990663dbd
GreckNavarro <130480169+GreckNavarro@users.noreply.github.com>
2023-06-08 10:05:45 -0500

    Lvl1-Lvl2

--- Assets/QuitarYMovement.cs
+++ Assets/QuitarYMovement.cs
@@ -8,7 +8,7 @@ public class QuitarYMovement : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            
+            other.GetComponent<BallController>().QuitarMovementY();
         }
     }
 }
