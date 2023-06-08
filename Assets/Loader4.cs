f197bd52906778f713695f6b52af0ee5fff99e7d
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-06-08 10:45:12 -0500

    Final attempt

--- Assets/Loader4.cs
+++ Assets/Loader4.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class Loader2 : MonoBehaviour
+public class Loader4: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRGBD = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            myRGBD.velocity = velocity * (-1);
+            myRGBD.useGravity = false;
+
+
+        }
     }
 }
