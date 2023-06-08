51a2040152257cfa14b8451709d91f91b1dc2093
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-06-08 10:37:59 -0500

    Load scenes

---
+++ Assets/Loader3.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Loader3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("GANO!");
+            SceneManager.LoadScene("level 3");
+
+        }
+    }
+}
--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FinishController : MonoBehaviour
 {
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+    void start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.CompareTag("Player")){
             Debug.Log("GANO!");
+            
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Player")){
-            Debug.Log("GANO!");
-        }
-    }
+    
+
 }
